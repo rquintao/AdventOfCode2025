2ffd7c662b2aa30b203630e9c2aa55fdc59c3e16
rquintao <ctw00766@criticaltechworks.com>
2025-12-01 19:16:38 +0000

    A1P2 done

--- src/main/java/org/rquintao/Day1.java
+++ src/main/java/org/rquintao/Day1.java
@@ -5,9 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import static java.lang.Math.abs;
+
 public class Day1 {
 
     private static final char RIGHT = 'R';
+
+    private Day1() {
+    }
 
     public static void first() throws IOException {
         try (InputStream inputStream = Day1.class.getResourceAsStream("/input1.txt");
@@ -22,7 +27,7 @@ public class Day1 {
                 if (RIGHT == line.charAt(0)) {
                     currentNumber += currentCommand;
                     if (currentNumber >= 100) {
-                        currentNumber = Math.abs(100 - currentNumber);
+                        currentNumber = abs(100 - currentNumber);
                     }
                 } else {
                     currentNumber -= currentCommand;
@@ -40,5 +45,54 @@ public class Day1 {
             IO.println("Error: " + e.getMessage());
         }
 
+    }
+
+    public static void second() throws IOException {
+        try (InputStream inputStream = Day1.class.getResourceAsStream("/input1.txt");
+             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            int currentNumber = 50;
+            int numberOnEntry;
+            int passwordCount = 0;
+            int currentCommand;
+            while ((line = br.readLine()) != null) {
+                currentCommand = Integer.parseInt(line.substring(1));
+                numberOnEntry = currentNumber;
+                if (RIGHT == line.charAt(0)) {
+                    currentNumber += currentCommand;
+                    if (currentNumber >= 100) {
+                        passwordCount = getPassWordCountForRightMove(numberOnEntry, passwordCount, currentCommand);
+                        currentNumber = currentNumber % 100;
+                    }
+                } else {
+                    currentNumber -= currentCommand;
+                    if (currentNumber <= 0) {
+                        passwordCount = getPasswordCountForLeftMove(numberOnEntry, passwordCount, currentCommand);
+                        currentNumber = 100 + (currentNumber % 100 == 0 ? -100 : currentNumber % 100);
+                    }
+                }
+            }
+            IO.println("Password Count: " + passwordCount);
+        } catch (IOException e) {
+            IO.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static int getPasswordCountForLeftMove(int numberOnEntry, int passwordCount, int currentCommand) {
+        if (numberOnEntry == 0) {
+            passwordCount += currentCommand / 100;
+        } else {
+            passwordCount += (abs((numberOnEntry - currentCommand)) / 100) + 1;
+        }
+        return passwordCount;
+    }
+
+    private static int getPassWordCountForRightMove(int numberOnEntry, int passwordCount, int currentCommand) {
+        if (numberOnEntry == 0) {
+            passwordCount += currentCommand / 100;
+        } else {
+            passwordCount += (numberOnEntry + currentCommand) / 100;
+        }
+        return passwordCount;
     }
 }
--- src/main/java/org/rquintao/Main.java
+++ src/main/java/org/rquintao/Main.java
@@ -2,7 +2,7 @@ import org.rquintao.Day1;
 
 
 static void main() throws IOException {
-        Day1.first();
+    Day1.second();
 }
 
 
